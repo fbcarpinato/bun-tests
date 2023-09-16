1b5048d2a44cade040abb84dc83eb39b30565107
Fabio Carpinato <fabiocarpinato@outlook.it>
2023-09-16 19:02:17 +0200

    feat: adds morgan logs

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,18 @@
 import express from 'express'
 import { PrismaClient } from '@prisma/client'
+import morgan from 'morgan'
+import fs from 'fs'
+import path from 'path'
 
 const prisma = new PrismaClient()
 
 const app = express()
+
+const accessLogStream = fs.createWriteStream(
+  path.join(__dirname, '../data/access.log'),
+  { flags: 'a' }
+)
+app.use(morgan('combined', { stream: accessLogStream }))
 
 app.get('/', async (request: express.Request, response: express.Response) => {
   const usersCount = await prisma.user.count()
