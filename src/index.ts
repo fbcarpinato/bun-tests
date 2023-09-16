0bc7e1f7767c922e609ce20061e26f48449c98dc
Fabio Carpinato <fabiocarpinato@outlook.it>
2023-09-16 19:32:07 +0200

    feat: adds actuator

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,8 @@ import path from 'path'
 import cors from 'cors'
 import compression from 'compression'
 import helmet from 'helmet'
+import enforce from 'express-sslify'
+import actuator from 'express-actuator'
 
 const prisma = new PrismaClient()
 
@@ -32,6 +34,16 @@ app.use(cors())
 app.use(compression())
 
 app.use(helmet())
+
+if (Bun.env.ENVIRONMENT === 'production') {
+  app.use(enforce.HTTPS({ trustProtoHeader: true }))
+}
+
+app.use(
+  actuator({
+    basePath: '/management'
+  })
+)
 
 app.get('/', async (request: express.Request, response: express.Response) => {
   const usersCount = await prisma.user.count()
