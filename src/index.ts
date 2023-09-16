bd17a34e95057cf8c9b991b317f5f327966465ff
Fabio Carpinato <fabiocarpinato@outlook.it>
2023-09-16 18:52:51 +0200

    chore: adds code formatting and linting via eslint and prettier

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
-import express from 'express';
-import { PrismaClient } from '@prisma/client';
+import express from 'express'
+import { PrismaClient } from '@prisma/client'
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient()
 
-const app = express();
+const app = express()
 
 app.get('/', async (request: express.Request, response: express.Response) => {
-    const usersCount = await prisma.user.count();
-    response.json({
-        data: {users: usersCount}
-    })
+  const usersCount = await prisma.user.count()
+  response.json({
+    data: { users: usersCount }
+  })
 })
 
 app.listen(3000, () => {
-    console.log('Listening to port 3000');
-});
+  console.log('Listening to port 3000')
+})
